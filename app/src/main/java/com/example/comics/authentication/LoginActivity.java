3b56ec12310ef06b18c28555757345d5bcf833c8
phukoika <tangtuphu@gmail.com>
2023-12-20 12:13:35 +0700

    custom toast login

--- app/src/main/java/com/example/comics/authentication/LoginActivity.java
+++ app/src/main/java/com/example/comics/authentication/LoginActivity.java
@@ -1,10 +1,15 @@
 package com.example.comics.authentication;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -59,6 +64,26 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
+    private void customToast(String content, String color, int imageResource) {
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.custom_toast, this.findViewById(R.id.custom_toast));
+
+        TextView textView = view.findViewById(R.id.tvToast);
+        textView.setText(content);
+
+        ImageView imageView = view.findViewById(R.id.icToast);
+        imageView.setImageResource(imageResource);
+
+        view.setBackgroundColor(Color.parseColor(color));
+
+        Toast toast = new Toast(this);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(view);
+        toast.setGravity(Gravity.TOP | Gravity.END, 16, 100);
+        toast.show();
+    }
+
+
 
     private void forgot() {
         Intent intent = new Intent(LoginActivity.this, ResetActivity.class);
@@ -76,30 +101,30 @@ public class LoginActivity extends AppCompatActivity {
         email = emailInput.getText().toString();
         password = passwordInput.getText().toString();
         if(TextUtils.isEmpty(email)) {
-            Toast.makeText(this, "Email is required", Toast.LENGTH_SHORT).show();
+            customToast("Email is required", "#FF0000", R.drawable.ic_error);
             return;
         }
         if (!isValidEmail(email)) {
-            Toast.makeText(this, "Invalid email address", Toast.LENGTH_SHORT).show();
+            customToast("Invalid email address", "#FF0000", R.drawable.ic_error);
             return;
         }
         if(TextUtils.isEmpty(password)) {
-            Toast.makeText(this, "Password is required", Toast.LENGTH_SHORT).show();
+            customToast("Password is required", "#FF0000", R.drawable.ic_error);
             return;
         }
         if (password.length() < 8) {
-            Toast.makeText(this, "Password must be at least 8 characters", Toast.LENGTH_SHORT).show();
+            customToast("Password must be at least 8 characters", "#FF0000", R.drawable.ic_error);
             return;
         }
         mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
-                    Toast.makeText(LoginActivity.this,"Login successfully", Toast.LENGTH_SHORT).show();
+                    customToast("Login successfully", "#FF299032", R.drawable.ic_check);
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     startActivity(intent);
                 }else {
-                    Toast.makeText(LoginActivity.this, "Login failed, please check your email or password again", Toast.LENGTH_SHORT).show();
+                    customToast("Login failed, please check your email or password again", "#FF0000", R.drawable.ic_error);
                 }
             }
         });
